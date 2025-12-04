e193b763359f3d20a37d326d5e6bd5ad1afba0f5
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-04 16:13:36 +0000

    🅼🅸🆂🆃 🐐 🅶🅾🅰🆃 just Upload asset .ico_images_assets.lua

---
+++ scripts/.ico_images_assets.lua
@@ -0,0 +1,5 @@
+return {
+    ["Github-Logo"] = { label = "Github-Logo", file = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/.ico/1764864797188_Github-Logo.ico" },
+    ["GitHub_Uploader"] = { label = "GitHub_Uploader", file = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/.ico/1764864798268_GitHub_Uploader.ico" },
+    ["logo-git-hub"] = { label = "logo-git-hub", file = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/.ico/1764864799361_logo-git-hub.ico" }
+}
