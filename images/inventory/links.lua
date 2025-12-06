2ee5a57e426afd3b5a8db7fae29d2a7c16a6e9f0
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-06 08:26:00 +0000

    Add links.lua - Generated links file

---
+++ images/inventory/links.lua
@@ -0,0 +1,111 @@
+-- FiveManage Images
+return {
+    ["meth_processed"] = {
+        label = "meth_processed",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/6_OtherMaterials/meth_processed.png"
+    },
+    ["meth_tray"] = {
+        label = "meth_tray",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/6_OtherMaterials/meth_tray.png"
+    },
+    ["weed_ak47_seed"] = {
+        label = "weed_ak47_seed",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/6_OtherMaterials/weed_ak47_seed.png"
+    },
+    ["weed_amnesia_seed"] = {
+        label = "weed_amnesia_seed",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/6_OtherMaterials/weed_amnesia_seed.png"
+    },
+    ["weed_nutriotion"] = {
+        label = "weed_nutriotion",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/6_OtherMaterials/weed_nutriotion.png"
+    },
+    ["weed_purple-haze_seed"] = {
+        label = "weed_purple-haze_seed",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/6_OtherMaterials/weed_purple-haze_seed.png"
+    },
+    ["weed_skunk_kush_seed"] = {
+        label = "weed_skunk_kush_seed",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/6_OtherMaterials/weed_skunk_kush_seed.png"
+    },
+    ["weed_skunk_seed"] = {
+        label = "weed_skunk_seed",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/6_OtherMaterials/weed_skunk_seed.png"
+    },
+    ["weed_white-widow_seed"] = {
+        label = "weed_white-widow_seed",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/6_OtherMaterials/weed_white-widow_seed.png"
+    },
+    ["gasoline"] = {
+        label = "gasoline",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/5_Materials/gasoline.png"
+    },
+    ["acetone"] = {
+        label = "acetone",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/4_Ingedients/acetone.png"
+    },
+    ["benzocaine"] = {
+        label = "benzocaine",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/4_Ingedients/benzocaine.png"
+    },
+    ["hydrochloridacid"] = {
+        label = "hydrochloridacid",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/4_Ingedients/hydrochloridacid.png"
+    },
+    ["methylamine"] = {
+        label = "methylamine",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/4_Ingedients/methylamine.png"
+    },
+    ["batterier"] = {
+        label = "batterier",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/3_Electronics/batterier.png"
+    },
+    ["batteries_new"] = {
+        label = "batteries_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/3_Electronics/batteries_new.png"
+    },
+    ["ENVI_batteries"] = {
+        label = "ENVI_batteries",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/3_Electronics/ENVI_batteries.png"
+    },
+    ["ENVI_trap_phone"] = {
+        label = "ENVI_trap_phone",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/3_Electronics/ENVI_trap_phone.png"
+    },
+    ["scales"] = {
+        label = "scales",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/3_Electronics/scales.png"
+    },
+    ["timer"] = {
+        label = "timer",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/3_Electronics/timer.png"
+    },
+    ["trap_phone"] = {
+        label = "trap_phone",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/3_Electronics/trap_phone.png"
+    },
+    ["yflip"] = {
+        label = "yflip",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/3_Electronics/yflip.png"
+    },
+    ["cement"] = {
+        label = "cement",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/0_Metals/6_OtherMetals/cement.png"
+    },
+    ["lithium"] = {
+        label = "lithium",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/0_Metals/6_OtherMetals/lithium.png"
+    },
+    ["iron"] = {
+        label = "iron",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/0_Metals/3_Iron/iron.png"
+    },
+    ["copper"] = {
+        label = "copper",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/0_Metals/1_Copper/copper.png"
+    },
+    ["aluminium"] = {
+        label = "aluminium",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/0_Metals/0_Aluminum/aluminium.png"
+    }
+}
