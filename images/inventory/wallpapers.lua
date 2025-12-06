2699d8444c660a5f0a7c4f2fa127dc6d09a23598
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-06 08:27:45 +0000

    Add wallpapers.lua - Generated links file

---
+++ images/inventory/wallpapers.lua
@@ -0,0 +1,19 @@
+-- FiveManage Images
+return {
+    ["autoExotic_wallpaper"] = {
+        label = "autoExotic_wallpaper",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/autoExotic_wallpaper.png"
+    },
+    ["yellowjack_daylight"] = {
+        label = "yellowjack_daylight",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/yellowjack_daylight.png"
+    },
+    ["yellowjack_mountains"] = {
+        label = "yellowjack_mountains",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/yellowjack_mountains.png"
+    },
+    ["yellowjack_wallpaperISH"] = {
+        label = "yellowjack_wallpaperISH",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/inventory/yellowjack_wallpaperISH.png"
+    }
+}
