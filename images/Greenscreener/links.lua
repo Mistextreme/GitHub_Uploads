15d28b49136e013ebe860a40eace4e80df402f73
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-06 14:48:05 +0000

    Add links.lua - Generated links file

---
+++ images/Greenscreener/links.lua
@@ -0,0 +1,7 @@
+-- FiveManage Images
+return {
+    ["fivemanage_urls"] = {
+        label = "fivemanage_urls",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/images/Greenscreener/fivemanage_urls.lua"
+    }
+}
