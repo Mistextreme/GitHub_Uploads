373bb10ccbfd7fec9fe88ef0ed91036a58993019
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-07 14:09:06 +0000

    Update videos.lua - Added 7 new entries

--- database/videos.lua
+++ database/videos.lua
@@ -1,3 +1,31 @@
 -- FiveManage Videos
 return {
+    ["grok_video_2025-12-07-13-47-43"] = {
+        label = "grok_video_2025-12-07-13-47-43",
+        video = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/grok_video_2025-12-07-13-47-43.mp4"
+    },
+    ["grok_video_2025-12-07-13-44-59"] = {
+        label = "grok_video_2025-12-07-13-44-59",
+        video = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/grok_video_2025-12-07-13-44-59.mp4"
+    },
+    ["grok_video_2025-12-07-13-40-38"] = {
+        label = "grok_video_2025-12-07-13-40-38",
+        video = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/grok_video_2025-12-07-13-40-38.mp4"
+    },
+    ["lol"] = {
+        label = "lol",
+        video = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/lol.mp4"
+    },
+    ["mistttt"] = {
+        label = "mistttt",
+        video = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/mistttt.mp4"
+    },
+    ["grok_video_2025-12-07-13-35-26"] = {
+        label = "grok_video_2025-12-07-13-35-26",
+        video = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/grok_video_2025-12-07-13-35-26.mp4"
+    },
+    ["grok_video_2025-12-07-13-30-37"] = {
+        label = "grok_video_2025-12-07-13-30-37",
+        video = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/grok_video_2025-12-07-13-30-37.mp4"
+    }
 }
