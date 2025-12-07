9fc991bd5bcea8a3eb2a45065548fdbe6e43dbc3
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-07 15:42:07 +0000

    Update gifs.lua - Added 10 new entries

--- database/gifs.lua
+++ database/gifs.lua
@@ -39,5 +39,45 @@ return {
     ["converted-1765112461559"] = {
         label = "converted-1765112461559",
         image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/converted-1765112461559.gif"
+    },
+    ["Intro-new_new"] = {
+        label = "Intro-new_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/Intro-new.gif"
+    },
+    ["Intro-new1_new"] = {
+        label = "Intro-new1_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/Intro-new1.gif"
+    },
+    ["New-intro2_new"] = {
+        label = "New-intro2_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/New-intro2.gif"
+    },
+    ["New-intro3_new"] = {
+        label = "New-intro3_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/New-intro3.gif"
+    },
+    ["New_intro_new"] = {
+        label = "New_intro_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/New_intro.gif"
+    },
+    ["converted-1765112461559_new"] = {
+        label = "converted-1765112461559_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/converted-1765112461559.gif"
+    },
+    ["grok_mist__1__new"] = {
+        label = "grok_mist__1__new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/grok_mist%20(1).gif"
+    },
+    ["grok_mist_new"] = {
+        label = "grok_mist_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/grok_mist.gif"
+    },
+    ["mist_code_intro_new"] = {
+        label = "mist_code_intro_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/mist_code_intro.gif"
+    },
+    ["mistttt_new"] = {
+        label = "mistttt_new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/mistttt.gif"
     }
 }
