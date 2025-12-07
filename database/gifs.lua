a60c3f266c63256b8ea6f9f5462d9f9bd8403db6
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-07 14:07:05 +0000

    Update gifs.lua - Added 10 new entries

--- database/gifs.lua
+++ database/gifs.lua
@@ -1,3 +1,43 @@
 -- FiveManage Gifs
 return {
+    ["New-intro3"] = {
+        label = "New-intro3",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/New-intro3.gif"
+    },
+    ["New-intro2"] = {
+        label = "New-intro2",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/New-intro2.gif"
+    },
+    ["Intro-new1"] = {
+        label = "Intro-new1",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/Intro-new1.gif"
+    },
+    ["Intro-new"] = {
+        label = "Intro-new",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/Intro-new.gif"
+    },
+    ["New_intro"] = {
+        label = "New_intro",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/New_intro.gif"
+    },
+    ["mistttt"] = {
+        label = "mistttt",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/mistttt.gif"
+    },
+    ["grok_mist__1_"] = {
+        label = "grok_mist__1_",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/grok_mist%20(1).gif"
+    },
+    ["grok_mist"] = {
+        label = "grok_mist",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/grok_mist.gif"
+    },
+    ["mist_code_intro"] = {
+        label = "mist_code_intro",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/mist_code_intro.gif"
+    },
+    ["converted-1765112461559"] = {
+        label = "converted-1765112461559",
+        image = "https://raw.githubusercontent.com/Mistextreme/GitHub_Uploads/main/assets/images/Grok/2025/12/07/converted-1765112461559.gif"
+    }
 }
